65e84303096cdd0912209169744594cdb2173be7
Sri Harish Popuri <26146242+sripopuri@users.noreply.github.com>
2025-11-28 21:25:18 -0600

    Added Positional Embeddings

---
+++ model.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import math
+
+
+class InputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.d_model = d_model
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # (seq_len, d_model) matrix for positional encodings
+        pe = torch.zeros(seq_len, d_model) 
+
+        # Create a tensor column vector of shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)  # Apply sin to even indices
+        pe[:, 1::2] = torch.cos(position * div_term)  # Apply cos to odd indices
+
+        pe = pe.unsqueeze(0)  # Shape (1, seq_len, d_model)
+        self.register_buffer('pe', pe)
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x + (self.pe[:, :x.size(1), :]).requires_grad_(False)
+        return self.dropout(x)
